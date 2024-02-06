abff099f801686bcefd561c9cc382960592eb340
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-06 06:42:36 +0330

    added keys for easy coding

---
+++ script.js
@@ -0,0 +1,4 @@
+let searchInput = document.querySelector("header input");
+let list = document.querySelector("ul");
+let addInput = document.querySelector("section input");
+let addBtn = document.querySelector("section button");
