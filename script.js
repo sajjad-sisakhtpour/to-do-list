93882da653ac679e3d40f507b5c6a2f078ee246c
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-06 07:21:46 +0330

    added delete option for delete items

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let searchInput = document.querySelector("header input");
-let list = document.querySelector("ul");
+let searchInput = document.querySelector("#search-form");
+let list = document.querySelector("#to-do-list");
 let addInput = document.querySelector("section input");
 let addBtn = document.querySelector("section button");
+
+list.addEventListener("click", (e) => {
+  if (e.target.nodeName == "SPAN" && e.target.className == "delete-btn") {
+    e.target.parentNode.remove();
+  }
+  if (list.children.length == 0) {
+    let eMsg = document.createElement("div");
+    eMsg.innerText = "your list is empty";
+    eMsg.style.color = "#333";
+    eMsg.id = "emMsg";
+    eMsg.style.textAlign='center'
+    list.appendChild(eMsg);
+  }
+});
